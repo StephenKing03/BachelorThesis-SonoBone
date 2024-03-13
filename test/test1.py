bf0a791f5f98dd9f348b459a54c49edf3f85ef86
StephenKing03 <koenigstephan91@gmail.com>
2024-03-13 14:16:03 +0100

    added sleep
    better distinction of positions

--- test/test1.py
+++ test/test1.py
@@ -1,11 +1,14 @@
 
 import mecademicpy.robot as mdr
+import time
+
 robot = mdr.Robot()
 robot.Connect(address='192.168.0.100')
 robot.ActivateRobot()
 robot.Home()
 
-robot.MoveJoints(0, 0, 0, 0, 0, 0)
+robot.MoveJoints(-45, 0, 0, 0, 0, 0)
+time.sleep(5)
 robot.MoveJoints(0, -60, 60, 0, 0, 0)
 
 robot.WaitIdle()
