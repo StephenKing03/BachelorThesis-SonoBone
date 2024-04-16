314fb105bc14ae1a309ce27d94b0a4a0f5038c60
StephenKing03 <koenigstephan91@gmail.com>
2024-04-16 17:35:26 +0200

    stepper_Test over vscode works

--- control/extra_components/stepper_receiver/stepper_receiver.ino
+++ control/extra_components/stepper_receiver/stepper_receiver.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Stepper.h>
 
+// Define the number of steps per revolution
+const int stepsPerRevolution = 200;
+
+// Create a Stepper object
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
+void setup() {
+  // Initialize the serial communication
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Check if there is any data available to read from the serial port
+  if (Serial.available() > 0) {
+    // Read the incoming value as an integer
+    int speed = Serial.parseInt();
+    speed = 1000;
 
+    // Set the motor speed
+    myStepper.setSpeed(speed);
+
+    // Rotate the motor continuously
+    myStepper.step(stepsPerRevolution);
+  }
 }
